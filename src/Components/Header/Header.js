3c8c52925addd5b54eae3976a394cc55fed7d3da
mamuntufayel <mamuntuffa@gmail.com>
2022-04-17 02:57:55 -0400

    login created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -20,11 +20,11 @@ const Header = () => {
             <Nav.Link href="home#weekendSpecial">Weekend Special</Nav.Link>
           </Nav>
           <Nav>
+            <Nav.Link as={Link} to="about">
+              About
+            </Nav.Link>
             <Nav.Link as={Link} to="login">
               Login
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const Login = () => {
   return (
     <div>
-      <h3>Please Login</h3>
+      <h2 className="text-primary text-center my-5 fs-2 fw-bolder">Login</h2>
+      <Form className="w-50 mx-auto">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Enter email" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Password" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 };
